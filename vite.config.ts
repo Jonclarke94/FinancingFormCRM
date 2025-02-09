0655049403edcea3feb5f40c05b80332eaef217b
Jonclarke94 <jonclarke94@gmail.com>
2025-02-09 12:23:55 -0600

    asdfsd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://jonclarke94.github.io/FinancingFormCRM/',
+  base: '/FinancingFormCRM/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
