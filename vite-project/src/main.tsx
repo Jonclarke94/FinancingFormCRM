d3506ce39d65c86be03487370cd728256976c3da
Jonclarke94 <jonclarke94@gmail.com>
2025-02-08 22:28:19 -0600

    config stuff

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -7,10 +7,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/*<ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-    */}
-    <App />
   </React.StrictMode>,
 );
