93ad91e8e3c832e3a1b49e67a5002442b180e6d9
Jonclarke94 <jonclarke94@gmail.com>
2025-02-09 11:59:07 -0600

    Move to root directory

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinancingForm from './components/FinancingForm';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+      <FinancingForm />
+    </div>
+  );
+}
+
+export default App;
