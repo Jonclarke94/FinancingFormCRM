e31342c03d287c26d52611cbf479b3c8ccf590db
Jonclarke94 <jonclarke94@gmail.com>
2025-02-09 12:18:42 -0600

    Github Pages Integration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://jonclarke94.github.io/FinancingFormCRM/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
